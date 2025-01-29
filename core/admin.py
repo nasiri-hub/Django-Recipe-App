0cfa8d70d8a66b6f03fca1d11a7d9f4da06339b4
nasiri-hub <193078316+nasiri-hub@users.noreply.github.com>
2025-01-29 22:52:28 +0330

    Improve admin page.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from tags.models import TaggedItem
+from recipe.admin import RecipeAdmin
+from recipe.models import Recipe
+from .models import User
 
-# Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('username', 'usable_password', 'password1', 'password2', 'email', 'first_name', 'last_name'),
+            },
+        ),
+    )
+    search_fields = ['first_name']
+
+
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    extra = 0
+    model = TaggedItem
+
+
+class CustomRecipeAdmin(RecipeAdmin):
+    inlines = [TagInline]
+
+
+admin.site.unregister(Recipe)
+admin.site.register(Recipe, CustomRecipeAdmin)
--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Recipe, Customer, Ingredient, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['category']
+
+    list_display = ['name', 'description',
+                    'time', 'price', 'link', 'instructions']
+    list_editable = ['price']
+    list_per_page = 10
+    list_select_related = ['category']
+    date_hierarchy = 'time'
+    ordering = ['name']
+
+    class Media:
+        css = {
+            'all': ['recipe/styles.css']
+        }
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering = ['user__first_name', 'user__last_name']
+    list_select_related = ['user']
+    search_fields = ['user__first_name__istartswith',
+                     'user__last_name__istartswith']
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['name']
